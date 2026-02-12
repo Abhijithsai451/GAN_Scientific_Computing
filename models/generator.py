ce359325eb11b7a83f111cf59ad75e995b944873
abhijithsai <abhijithsaithirunahari51@gmail.com>
2026-02-12 09:59:50 +0100

    Base line DCGAN Architecture is implemented.

--- models/generator.py
+++ models/generator.py
@@ -2,37 +2,47 @@ import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
-    def __init__(self, n_classes, latent_dim, embedding_dim):
+    def __init__(self, config):
         super(Generator, self).__init__()
-        self.n_classes = n_classes
-        self.label_embedding = nn.Embedding(n_classes, embedding_dim)
-        self.init_layer = nn.Sequential(
-            nn.Linear(latent_dim + embedding_dim, 512 * 4 * 4),
-            nn.ReLU(True),
-        )
-        self.upsample = nn.Sequential(
-            #1. Input: 512 x 4 x 4 --> Output: 256 x 8 x 8 (Latent Dimension to the Conv Output)
-            nn.ConvTranspose2d(512, 256, 4, 2, 1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            # 1. Input: 256 x 8 x 8 --> Output: 128 x 16 x 16 (Layer 1  to the Conv Output)
-            nn.ConvTranspose2d(256, 128, 4, 2, 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
+        arch = config.model
+        self.latent_dim = arch['latent_dim']
+        # Class Embeddings: Turns the labels into a dense vector
+        embedding_dim = arch['embedding_dim']
+        self.num_classes = arch['num_classes']
+        self.label_embedding = nn.Embedding(self.num_classes,embedding_dim)
 
-            nn.ConvTranspose2d(128, 64, 4, 2, 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-
-            # Final Layer
-            nn.ConvTranspose2d(64,3,4,2,1),
-            nn.Tanh()
+        # Initial Block: (latent_dim + Embeddings) --> 256 * 4 * 4
+        self.init_block = nn.Sequential(
+            nn.ConvTranspose2d(self.latent_dim,self.label_embedding,kernel_size=4,stride=2,padding=1),
+            nn.BatchNorm2d(arch['generator']['initial_channels']),
+            nn.ReLU(True)
         )
 
-        def forward(self,noise,  x):
-            label_embedding = self.label_embedding(x)
-            gen_input = torch.cat((noise, label_embedding), dim=1)
-            out = self.init_layer(gen_input)
-            out = out.view(-1, 512,4,4)
-            img = self.upsample(out)
-            return img
+        # Hidden Layers
+        layers = []
+        current_channel = arch['generator']['initial_channels']
+        channels = arch['generator']['channel_sequence']
+
+        for next_channel in channels:
+            layers.append(nn.ConvTranspose2d(current_channel,next_channel,kernel_size=4,stride=2,padding=1))
+            layers.append(nn.BatchNorm2d(next_channel))
+            layers.append(nn.ReLU(True))
+            current_channel = next_channel
+
+        layers.append(nn.ConvTranspose2d(current_channel,out_channels = 3,kernel_size=4,stride=2,padding=1))
+        layers.append(nn.Tanh()) # Here the activation function ensures the output is in range of [-1,1]
+
+        self.generator = nn.Sequential(*layers)
+
+    def forward(self, input, labels):
+        x = input.view(-1, self.latent_dim, 1,1)
+        label = self.label_embedding(labels).unsqueeze(2).unsqueeze(3)
+        x = torch.cat([x, label],1)
+
+        x = self.init_block(x)
+        gen_images = self.generator(x)
+        return gen_images
+
+
+
+
---
+++ models/model_utils.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+def weight_init(m):
+    classname = m.__class__.__name__
+    if "Conv" in classname:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif "BatchNorm" in classname:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+    elif 'Linear' in classname:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if m.bias is not None:
+            nn.init.constant_(m.bias.data, 0.0)
